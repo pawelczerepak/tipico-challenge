06c55a9d0fbbd41da47caa42bcaccf928b00959d
pawel <pwcz88@gmail.com>
2019-10-02 13:55:14 +0200

    added search reducer with tests

---
+++ src/reducers/__tests__/search.ts
@@ -0,0 +1,103 @@
+import reducer from '../search';
+import {SEARCH_REPOSITORIES, SEARCH_REPOSITORIES_FAIL, SEARCH_REPOSITORIES_SUCCESS} from '../../constants/ActionTypes';
+
+const initialState = {
+    totalCount: 0,
+    params: {},
+    pages: {},
+    error: null,
+};
+
+describe('search reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {type: ''}))
+            .toEqual({
+                initialState,
+            });
+    });
+
+    it('should handle SEARCH_REPOSITORIES', () => {
+        const params = {
+            query: 'query',
+            resultsPerPage: 10,
+            page: 1,
+            order: 'desc',
+            sort: 'stars',
+        };
+        expect(
+            reducer(initialState, {
+                type: SEARCH_REPOSITORIES,
+                meta: {
+                    params,
+                },
+            }),
+        )
+            .toEqual({
+                ...initialState,
+                params,
+            });
+    });
+
+    it('should handle SEARCH_REPOSITORIES_SUCCESS', () => {
+        const params = {
+            query: 'query',
+            resultsPerPage: 10,
+            page: 1,
+            order: 'desc',
+            sort: 'stars',
+        };
+        // tslint:disable:object-literal-key-quotes quotemark trailing-comma
+        const items = [
+            {
+                "full_name": "chvin/react-tetris",
+                "owner": {
+                    "login": "chvin",
+                    "avatar_url": "https://avatars2.githubusercontent.com/u/5383506?v=4",
+                    "url": "https://api.github.com/users/chvin",
+                },
+                "description": "Use React, Redux, Immutable to code Tetris. 🎮",
+                "url": "https://api.github.com/repos/chvin/react-tetris",
+                "stargazers_count": 5592,
+                "language": "JavaScript",
+            },
+        ];
+        // tslint:enable
+        const totalCount = 1;
+        expect(
+            reducer(initialState, {
+                type: SEARCH_REPOSITORIES_SUCCESS,
+                meta: {
+                    params,
+                },
+                payload: {
+                    data: {
+                        total_count: totalCount,
+                        items,
+                    },
+                },
+            }),
+        )
+            .toEqual({
+                ...initialState,
+                params,
+                pages: {
+                    1: items,
+                },
+                totalCount,
+            });
+    });
+
+    it('should handle SEARCH_REPOSITORIES_FAIL', () => {
+        const error = new Error('error');
+        expect(
+            reducer(initialState, {
+                type: SEARCH_REPOSITORIES_FAIL,
+                error,
+            }),
+        )
+            .toEqual({
+                ...initialState,
+                error,
+            });
+    });
+});
---
+++ src/reducers/search.ts
@@ -0,0 +1,43 @@
+import {AxiosAction, Params, Response} from '../types';
+
+const initialState = {
+    totalCount: 0,
+    params: {},
+    pages: {},
+    error: null,
+};
+export default function search(state: any = initialState, {type, payload, error, meta}: AxiosAction) {
+    switch (type) {
+        case 'SEARCH_REPOSITORIES': {
+            const {params}: { params: Params } = meta;
+            return ({
+                ...state,
+                params,
+                pages: state.params.query !== params.query ? {} : state.pages,
+                error: null,
+                totalCount: 0,
+            });
+        }
+        case 'SEARCH_REPOSITORIES_SUCCESS': {
+            const {data}: { data: Response } = payload;
+            const {params}: { params: Params } = meta;
+
+            return ({
+                ...state,
+                pages: {
+                    ...state.pages,
+                    [params.page]: data.items,
+                },
+                totalCount: data.total_count,
+            });
+        }
+        case 'SEARCH_REPOSITORIES_FAIL': {
+            return ({
+                ...state,
+                error,
+            });
+        }
+        default:
+            return state;
+    }
+}
